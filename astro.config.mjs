16d2e2738b81c4df8ee5d3d0e72ecbcd8da1fe30
Julia <60198903+ninjulia@users.noreply.github.com>
2024-03-02 15:35:01 -0500

    implement Image components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
-
 import sitemap from '@astrojs/sitemap';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://astro-storyblock-portfolio-template.netlify.app',
 	integrations: [
 		mdx(),
 		sitemap(),
@@ -16,4 +17,6 @@ export default defineConfig({
 			},
 		}),
 	],
+	output: 'server',
+	adapter: netlify(),
 });
