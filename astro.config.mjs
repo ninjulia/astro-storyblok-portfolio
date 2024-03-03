726a76dfae9e82d0437cdacbe0437ec043284c7e
Julia <60198903+ninjulia@users.noreply.github.com>
2024-03-02 19:53:49 -0500

    add storyblok components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
-import netlify from '@astrojs/netlify';
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,7 +19,27 @@ export default defineConfig({
 				feather: ['*'],
 			},
 		}),
+		storyblok({
+			accessToken: env.STORYBLOK_TOKEN,
+			components: {
+				blogPost: 'storyblok/BlogPost',
+				blogPostList: 'storyblok/BlogPostList',
+				page: 'storyblok/Page',
+				About: 'storyblok/About',
+				FullWidthFeature: 'storyblok/FullWidthFeature',
+				Hero: 'storyblok/Hero',
+				StandardFeature: 'storyblok/StandardFeature',
+				Testimonial: 'storyblok/Testimonial',
+			},
+			apiOptions: {
+				region: 'us',
+			},
+		}),
 	],
-	output: 'server',
-	adapter: netlify(),
+	vite: {
+		plugins: [basicSsl()],
+		server: {
+			https: true,
+		},
+	},
 });
