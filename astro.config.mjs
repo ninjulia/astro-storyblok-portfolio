32d4e188afeceff6881c0d618c454a337494831c
Julia <60198903+ninjulia@users.noreply.github.com>
2024-03-03 17:54:32 -0500

    set up ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,47 +5,45 @@ import sitemap from '@astrojs/sitemap';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
-
+import netlify from "@astrojs/netlify";
 const env = loadEnv('', process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-storyblock-portfolio-template.netlify.app',
-	integrations: [
-		mdx(),
-		sitemap(),
-		icon({
-			include: {
-				feather: ['*'],
-			},
-		}),
-		storyblok({
-			accessToken: env.STORYBLOK_TOKEN,
-			components: {
-				blogPost: 'storyblok/BlogPost',
-				blogPostList: 'storyblok/BlogPostList',
-				page: 'storyblok/Page',
-				Config: 'storyblok/Config',
-				About: 'storyblok/About',
-				FullWidthFeature: 'storyblok/FullWidthFeature',
-				Hero: 'storyblok/Hero',
-				StandardFeature: 'storyblok/StandardFeature',
-				Testimonial: 'storyblok/Testimonial',
-				Overview: 'storyblok/Overview',
-				FormWrapper: 'storyblok/FormWrapper',
-				FormInput: 'storyblok/FormInput',
-				FormTextarea: 'storyblok/FormTextarea',
-				FormCheckbox: 'storyblok/FormCheckbox',
-			},
-			apiOptions: {
-				region: 'us',
-			},
-		}),
-	],
-	vite: {
-		plugins: [basicSsl()],
-		server: {
-			https: true,
-		},
-	},
+  site: 'https://astro-storyblock-portfolio-template.netlify.app',
+  integrations: [mdx(), sitemap(), icon({
+    include: {
+      feather: ['*']
+    }
+  }), storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      blogPost: 'storyblok/BlogPost',
+      blogPostList: 'storyblok/BlogPostList',
+      page: 'storyblok/Page',
+      Config: 'storyblok/Config',
+      About: 'storyblok/About',
+      FullWidthFeature: 'storyblok/FullWidthFeature',
+      Hero: 'storyblok/Hero',
+      StandardFeature: 'storyblok/StandardFeature',
+      Testimonial: 'storyblok/Testimonial',
+      Overview: 'storyblok/Overview',
+      FormWrapper: 'storyblok/FormWrapper',
+      FormInput: 'storyblok/FormInput',
+      FormTextarea: 'storyblok/FormTextarea',
+      FormCheckbox: 'storyblok/FormCheckbox'
+    },
+    apiOptions: {
+      region: 'us'
+    }
+  })],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true
+    }
+  },
+  output: "server",
+  adapter: netlify()
 });
